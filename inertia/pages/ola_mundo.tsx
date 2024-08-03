d64043a3275d970fd42c7e792927007df19da05e
Leandro Bataglia <leandro.bataglia@gmail.com>
2024-08-03 08:15:11 -0300

    [feat] add Tailwind to Project

---
+++ inertia/pages/ola_mundo.tsx
@@ -0,0 +1,13 @@
+import { Head } from '@inertiajs/react'
+
+export default function OlaMundo() {
+  return (
+    <>
+      <Head title="Olá Mundo" />
+
+      <div className="container">
+        <div className="text-indigo-900 text-right underline"> Olá Mundo </div>
+      </div>
+    </>
+  )
+}
